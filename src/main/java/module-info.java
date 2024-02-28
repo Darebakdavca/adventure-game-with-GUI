668118986597f183b47dfebc520e443933f350b8
David Brablc <brad14@vse.cz>
2024-02-28 12:34:14 +0100

    Přidání nápověda

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module cz.vse.semestralkaadventurabrad14 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
 
     opens cz.vse.semestralkaadventurabrad14.main to javafx.fxml;
